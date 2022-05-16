187a289631dcc96037098f4795f45fcc789ae9d3
caldev <caldart123@gmail.com>
2022-05-15 21:35:59 -0300

    reacting to events

--- events/nats.go
+++ events/nats.go
@@ -58,3 +58,39 @@ func (n *NatsEventStore) PublishCreatedFeed(ctx context.Context, feed *models.Fe
 	}
 	return n.conn.Publish(msg.Type(), data)
 }
+
+func (n *NatsEventStore) decodeMessage(data []byte, m interface{}) error {
+	b := bytes.Buffer{}
+	b.Write(data)
+	return gob.NewDecoder(&b).Decode(m)
+}
+
+func (n *NatsEventStore) OnCreatedFeed(ctx context.Context, f func(CreatedFeedMessage)) (err error) {
+	msg := CreatedFeedMessage{}
+	n.feedCreatedSub, err = n.conn.Subscribe(msg.Type(), func(m *nats.Msg) {
+		n.decodeMessage(m.Data, &msg)
+		f(msg)
+	})
+	return
+}
+
+func (n *NatsEventStore) SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	m := CreatedFeedMessage{}
+	n.feedCreatedChan = make(chan CreatedFeedMessage, 64)
+	ch := make(chan *nats.Msg, 64)
+	var err error
+	n.feedCreatedSub, err = n.conn.ChanSubscribe(m.Type(), ch)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			select {
+			case msg := <-ch:
+				n.decodeMessage(msg.Data, &m)
+				n.feedCreatedChan <- m
+			}
+		}
+	}()
+	return (<-chan CreatedFeedMessage)(n.feedCreatedChan), nil
+}
